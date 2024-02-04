b2246af9d685dd3242b81fed65703c37fb540731
koyuul <shannonkyle.deleon@gmail.com>
2024-02-03 21:13:54 -0800

    Cleaning up residual files

--- HeadBoard/Scheduler/Scheduler.cpp
+++ HeadBoard/Scheduler/Scheduler.cpp
@@ -1,15 +1,3 @@
-/*
-    Todo:
-        User control (for getting/setting commands)
-        - SetScheduleUpcoming()
-        - SetScheduleRepeating()
-        - GetScheduleUpcoming()
-        - GetScheduleRepeating()
-
-        Internal commands
-        - Forward()
-*/
-
 int main(int argc, char const *argv[])
 {
     /* code */
