2ed5a268d5024a8ede67ff30a99c05ac53eccb9a
koyuul <shannonkyle.deleon@gmail.com>
2024-02-12 07:25:04 -0800

    Moves pipe variables into it's own struct

--- HeadBoard/CommandHandler.cpp
+++ HeadBoard/CommandHandler.cpp
@@ -9,35 +9,35 @@
 #include <sys/types.h>  // Used for extra type capabilities
 #include <unistd.h>     // Used for extra type capabilities
 
-// TODO: make a class for pipes
+struct Pipe {
+    int status;
+    const char *path;
+    char incoming[128], outgoing[128];
+    Pipe(const char *p) : path(p){
+        mkfifo(path, 0666);
+    }
+};
 
 int main(int argc, char const *argv[])
 {
-    int radio_to_main_pipe;
-    // FIFO file path
-    const char * pipe_path = "/tmp/radio_to_main_pipe";
+    Pipe radio_to_main("/tmp/radio_to_main_pipe");
 
-    // Creating the named file(FIFO)
-    // mkfifo(<pathname>,<permission>)
-    mkfifo(pipe_path, 0666);
-     
-    char str1[80], str2[80];
     while (1)
     {
         // First open in read only and read
-        radio_to_main_pipe = open(pipe_path,O_RDONLY);
-        read(radio_to_main_pipe, str1, 80);
+        radio_to_main.status = open(radio_to_main.path, O_RDONLY);
+        read(radio_to_main.status, radio_to_main.incoming, 128);
  
         // Print the read string and close
-        printf("From Radio: %s\n", str1);
-        close(radio_to_main_pipe);
+        printf("From Radio: %s\n", radio_to_main.incoming);
+        close(radio_to_main.status);
  
         // Now open in write mode and write
         // string taken from user.
-        radio_to_main_pipe = open(pipe_path,O_WRONLY);
-        fgets(str2, 80, stdin);
-        write(radio_to_main_pipe, str2, strlen(str2)+1);
-        close(radio_to_main_pipe);
+        radio_to_main.status = open(radio_to_main.path, O_WRONLY);
+        fgets(radio_to_main.outgoing, 80, stdin);
+        write(radio_to_main.status, radio_to_main.outgoing, strlen(radio_to_main.outgoing)+1);
+        close(radio_to_main.status);
     }
     return 0;
 }
