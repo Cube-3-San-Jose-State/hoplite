345e4efb2dbdb54e5b9ec523a96d9410ce277260
koyuul <shannonkyle.deleon@gmail.com>
2024-02-23 20:28:31 -0800

    Update receiver code

--- HeadBoard/Radio/Receiver/Receiver.ino
+++ HeadBoard/Radio/Receiver/Receiver.ino
@@ -1,53 +1,46 @@
-// Feather9x_TX
-// -*- mode: C++ -*-
-// Example sketch showing how to create a simple messaging client (transmitter)
-// with the RH_RF95 class. RH_RF95 class does not provide for addressing or
-// reliability, so you should only use RH_RF95 if you do not need the higher
-// level messaging abilities.
-// It is designed to work with the other example Feather9x_RX
-
+// RFM9x Receiver code
 #include <SPI.h>
 #include <RH_RF95.h>
+// JSON Handler code
+#include <Arduino_JSON.h>
 
+// Define pins and frequecy
 #define RFM95_CS    8
 #define RFM95_INT   3
 #define RFM95_RST   4
 
 #define RF95_FREQ 915.0
 
+// Instantiate
 RH_RF95 rf95(RFM95_CS, RFM95_INT);
 
 void setup() {
   pinMode(RFM95_RST, OUTPUT);
   digitalWrite(RFM95_RST, HIGH);
 
+  // Start serial
   Serial.begin(115200);
-  while (!Serial) delay(1);
   delay(100);
 
-  Serial.println("Feather LoRa TX Test!");
-
-  // manual reset
+  // Start radio
+  Serial.println("Starting radio...");
   digitalWrite(RFM95_RST, LOW);
   delay(10);
   digitalWrite(RFM95_RST, HIGH);
   delay(10);
 
   while (!rf95.init()) {
-    Serial.println("LoRa radio init failed");
-    Serial.println("Uncomment '#define SERIAL_DEBUG' in RH_RF95.cpp for detailed debug info");
+    Serial.println("Radio init failed");
     while (1);
   }
-  Serial.println("LoRa radio init OK!");
+  Serial.println("Radio init OK!");
 
-  // Defaults after init are 434.0MHz, modulation GFSK_Rb250Fd250, +13dbM
+  // Changes frequency from default to 915.0MHz
   if (!rf95.setFrequency(RF95_FREQ)) {
     Serial.println("setFrequency failed");
     while (1);
   }
   Serial.print("Set Freq to: "); Serial.println(RF95_FREQ);
-
-  // Defaults after init are 434.0MHz, 13dBm, Bw = 125 kHz, Cr = 4/5, Sf = 128chips/symbol, CRC on
 
   // The default transmitter power is 13dBm, using PA_BOOST.
   // If you are using RFM95/96/97/98 modules which uses the PA_BOOST transmitter pin, then
@@ -55,23 +48,20 @@ void setup() {
   rf95.setTxPower(23, false);
 }
 
-int16_t packetnum = 0;  // packet counter, we increment per xmission
-
 void loop() {
   delay(1000); // Wait 1 second between transmits, could also 'sleep' here!
-  Serial.println("Transmitting..."); // Send a message to rf95_server
+  JSONVar test_packet;
+  test_packet["command"] = "R_T";
+  test_packet["parameter"] = (int) 1111111;
+  String test_packet_string = JSON.stringify(test_packet);
+  Serial.print(test_packet_string);
 
-  char radiopacket[20] = "Hello World #      ";
-  itoa(packetnum++, radiopacket+13, 10);
-  Serial.print("Sending: "); 
-  Serial.println(radiopacket);
-  radiopacket[19] = 0;
+  Serial.println("Transmitting basic message..."); // Send a message to rf95_server
 
-  Serial.println("Sending...");
+  // char radiopacket[20] = "Basic send";
+  Serial.print("Sending:"); 
+  Serial.println(test_packet_string);
+
   delay(10);
-  rf95.send((uint8_t *)radiopacket, 20);
-
-  Serial.println("Waiting for packet to complete...");
-  delay(10);
-  rf95.waitPacketSent();
+  rf95.send((uint8_t *)test_packet_string.c_str(), test_packet_string.length());
 }
