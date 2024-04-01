01f487eb31eaf52d9565af84c0235de4f7ab11d0
daveslh <david.sl.huang@gmail.com>
2024-04-01 02:19:52 -0700

    JSON parser to convert incoming and outgoing strings into JSON objects of type double, int, and string.

---
+++ HeadBoard/AstraeusHandler/StringToJson.cpp
@@ -0,0 +1,102 @@
+/**
+ * JSON Parser for incoming and outgoing Pipes. Converts to double, then int, then strings.
+ * Responsiblities: Converts incoming and outgoing strings into JSON objects of type double, int, and string.
+*/
+
+// include statements from C/C++ library and/or Astraeus
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <unordered_map>
+
+bool isWhitespace(char ch) {
+    return ch == ' ' || ch == '\t' || ch == '\n';
+}
+
+std::string extractString(std::string& str, int& i) {
+    i++;
+    std::string value;
+    while(i < str.length() && str[i] != '"') {
+        value += str[i];
+        i++;
+    }
+    i++;
+    return value;
+}
+
+std::pair<std::string, std::any> parseKeyValuePair(std::string& str, int& i) {
+    while(isWhitespace(str[i])) {
+        i++;
+    }
+
+    std::string key = extractString(str, i);
+
+    if (str[i] != ':') {
+        return {"", ""};
+    }
+    i++;
+
+    while(isWhitespace(str[i])) {
+        i++;
+    }
+
+    std::string initialValue = extractString(str, i);
+    std::any value;
+    
+    try {
+        // Attempt to convert to double
+        value = std::stod(initialValue);
+    } catch (const std::invalid_argument&) {
+        try {
+        // If double conversion fails, try int
+        value = std::stoi(initialValue);
+        } catch (const std::invalid_argument&) {
+        // If both conversions fail, keep as string
+        value = initialValue;
+        }
+    }
+
+    return {key,value};
+}
+
+std::unordered_map<std::string, std::any> stringToJson(std::string& str) {
+    std::unordered_map<std::string, std::any> json;
+    int i = 0;
+
+    if (str[i] != '{') {
+        return json;
+    }
+    i++;
+
+    while (i < str.length() && str[i] != '}') {
+        
+        std::pair<std::string, std::any> keyValue = parseKeyValuePair(str, i);
+        
+        if (!keyValue.first.empty()) {
+            json[keyValue.first] = keyValue.second;
+        }
+        while (isWhitespace(str[i])) {
+            i++;
+        }
+        if (str[i] == ',') {
+            i++;
+        }
+    }
+
+    if (str[i] != '}') {
+        return json;
+    }
+    i++;
+
+    return json;
+}
+
+// Driver
+int main(int argc, char *argv[])
+{
+    std::string json = "{\"process\":\"ASTRAEUS\",\"dir\":\"OUTGOING\",\"function\":\"REQ_TEL\",\"arguments\":[\"11111111\"]}";
+    std::unordered_map<std::string,std::any> s = stringToJson(json);
+     for (auto& keyValue : s) {
+      std::cout << keyValue.first << ": " << keyValue.second << std::endl;
+    }
+}
